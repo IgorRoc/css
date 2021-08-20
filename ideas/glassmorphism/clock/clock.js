d68f895484bf799c8e92ac67481a33dd419c451a
IgorRoc <igor_roc@hotmail.com.br>
2021-08-20 17:55:01 -0300

    [style]: Prettier

--- ideas/glassmorphism/clock/clock.js
+++ ideas/glassmorphism/clock/clock.js
@@ -8,9 +8,9 @@ let hh = day.getHours()
 let mm = day.getMinutes()
 let ss = day.getSeconds()
 
-hr.style.setProperty("--deslocamento", `${360*hh/24}deg`)
-mn.style.setProperty("--deslocamento", `${360*mm/60}deg`)
-sc.style.setProperty("--deslocamento", `${360*ss/60}deg`)
+hr.style.setProperty("--deslocamento", `${(360 * hh) / 24}deg`)
+mn.style.setProperty("--deslocamento", `${(360 * mm) / 60}deg`)
+sc.style.setProperty("--deslocamento", `${(360 * ss) / 60}deg`)
 
 console.log(hh, mm, ss)
-console.log(hh+(mm/12), 360*mm/60, 360*ss/60)
+console.log(hh + mm / 12, (360 * mm) / 60, (360 * ss) / 60)
