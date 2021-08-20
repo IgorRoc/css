56d2732288c07c786c8156a9f4258ac94edd2a57
IgorRoc <igor_roc@hotmail.com.br>
2021-08-20 19:27:59 -0300

    [fix]: Responsive

--- ideas/glassmorphism/battery/battery.js
+++ ideas/glassmorphism/battery/battery.js
@@ -5,32 +5,36 @@ let outOfCharge = document.querySelector("#outOfCharge")
 let fullCharge = document.querySelector("#fullCharge")
 
 slider.addEventListener("change", (e) => {
-    progress.style.width = slider.value + "%"
-    batteryValue.innerText = slider.value + "%"
-    if(slider.value <= 0){
-        outOfCharge.classList.remove("hide")
-    }else if(slider.value >= 100){
-        fullCharge.classList.remove("hide")
-    }else{
-        outOfCharge.classList.add("hide")
-        fullCharge.classList.add("hide")
-    }
+	progress.style.width = slider.value + "%"
+	batteryValue.innerText = slider.value + "%"
+	if (slider.value <= 0) {
+		outOfCharge.classList.remove("hide")
+		fullCharge.classList.add("hide")
+	} else if (slider.value >= 100) {
+		fullCharge.classList.remove("hide")
+		outOfCharge.classList.add("hide")
+	} else {
+		outOfCharge.classList.add("hide")
+		fullCharge.classList.add("hide")
+	}
 })
 
 getBatteryPercent()
-function getBatteryPercent(){
-    console.log("entrou")
-    navigator.getBattery().then((bat) => {
-        progress.style.width = bat.level*100 + "%"
-        batteryValue.innerText = bat.level*100 + "%"
-        slider.value = bat.level*100
-        if(bat.level <= 0){
-            outOfCharge.classList.remove("hide")
-        }else if(bat.level >= 1){
-            fullCharge.classList.remove("hide")
-        }else{
-            outOfCharge.classList.add("hide")
-            fullCharge.classList.add("hide")
-        }
-    })
+function getBatteryPercent() {
+	console.log("entrou")
+	navigator.getBattery().then((bat) => {
+		progress.style.width = bat.level * 100 + "%"
+		batteryValue.innerText = bat.level * 100 + "%"
+		slider.value = bat.level * 100
+		if (bat.level <= 0) {
+			outOfCharge.classList.remove("hide")
+			fullCharge.classList.add("hide")
+		} else if (bat.level >= 1) {
+			fullCharge.classList.remove("hide")
+			outOfCharge.classList.add("hide")
+		} else {
+			outOfCharge.classList.add("hide")
+			fullCharge.classList.add("hide")
+		}
+	})
 }
