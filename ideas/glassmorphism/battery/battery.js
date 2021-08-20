5b6e217e177acee518a66a5df092c4ffbd47e5eb
IgorRoc <igor_roc@hotmail.com.br>
2021-08-20 17:55:13 -0300

    [new]: Battery

---
+++ ideas/glassmorphism/battery/battery.js
@@ -0,0 +1,36 @@
+let progress = document.querySelector("#progress")
+let batteryValue = document.querySelector("#batteryValue")
+let slider = document.querySelector("#sliderBar")
+let outOfCharge = document.querySelector("#outOfCharge")
+let fullCharge = document.querySelector("#fullCharge")
+
+slider.addEventListener("change", (e) => {
+    progress.style.width = slider.value + "%"
+    batteryValue.innerText = slider.value + "%"
+    if(slider.value <= 0){
+        outOfCharge.classList.remove("hide")
+    }else if(slider.value >= 100){
+        fullCharge.classList.remove("hide")
+    }else{
+        outOfCharge.classList.add("hide")
+        fullCharge.classList.add("hide")
+    }
+})
+
+getBatteryPercent()
+function getBatteryPercent(){
+    console.log("entrou")
+    navigator.getBattery().then((bat) => {
+        progress.style.width = bat.level*100 + "%"
+        batteryValue.innerText = bat.level*100 + "%"
+        slider.value = bat.level*100
+        if(bat.level <= 0){
+            outOfCharge.classList.remove("hide")
+        }else if(bat.level >= 1){
+            fullCharge.classList.remove("hide")
+        }else{
+            outOfCharge.classList.add("hide")
+            fullCharge.classList.add("hide")
+        }
+    })
+}
